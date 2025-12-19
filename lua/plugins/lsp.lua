bb29d635dcc93996c947961da531abb4c4bb60dc
Emmanems567 <emmanuelalejandro.adame@gmail.com>
2025-12-18 18:22:19 -0600

    Implemented windows compatibility

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,123 +1,117 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            -- El núcleo de Java (Debe ser dependencia para cargar antes)
-            "nvim-java/nvim-java",
-            
-            -- Gestores de instalación
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            
-            -- Autocompletado
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/nvim-cmp",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
-        config = function()
-            -- =========================================================
-            -- 1. INICIAR JAVA PRIMERO (REGLA DE ORO)
-            -- =========================================================
-            -- Esto inyecta la lógica de nvim-java en lspconfig
-            require("java").setup()
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"nvim-java/nvim-java",
 
-            -- =========================================================
-            -- 2. CONFIGURAR MASON
-            -- =========================================================
-            require("mason").setup()
-            
-            -- =========================================================
-            -- 3. CONFIGURAR OTROS LENGUAJES (Mason-LSPConfig)
-            -- =========================================================
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 
-            require("mason-lspconfig").setup({
-                -- NO pongas jdtls aquí, nvim-java lo gestiona
-                ensure_installed = {
-                    "lua_ls",
-                    "html",
-                    "cssls",
-                    "clangd",
-                    "ts_ls",
-                    "omnisharp",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			require("java").setup()
+
+			require("mason").setup()
+
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"html",
+					"cssls",
+					"clangd",
+					"ts_ls",
+					"omnisharp",
+				},
+				handlers = {
+					function(server_name)
+						lspconfig[server_name].setup({
+							capabilities = capabilities,
+						})
+					end,
+
+					["lua_ls"] = function()
+						lspconfig.lua_ls.setup({
+							capabilities = capabilities,
+							settings = {
+								Lua = { diagnostics = { globals = { "vim" } } },
+							},
+						})
+					end,
+				},
+			})
+
+      local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
+
+      local java8_path = ""
+      local java_latest_path = ""
+
+      if is_windows then
+        java_latest_path = "C:\\Program Files\\Amazon Coretto\\jdk21.0.3_9"
+        java8_path = "C:\\ProgramData\\Oracle\\Java\\javapath"
+      else
+        java_latest_path = "/usr/lib/jvm/java-25-openjdk"
+        java8_path = "/usr/lib/jvm/java-8-openjdk"
+      end
+
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+				settings = {
+					java = {
+						configuration = {
+							runtimes = {
+								{
+									name = "JavaSE-25",
+									path = java_latest_path,
+									default = true,
+								},
+                {
+                  name = "JavaSE-21",
+                  path = java_latest_path,
+                  default = true,
                 },
-                handlers = {
-                    -- Configuración automática para todo lo que NO sea Java
-                    function(server_name)
-                        lspconfig[server_name].setup({
-                            capabilities = capabilities,
-                        })
-                    end,
-                    
-                    -- Excepción para Lua (Variable global 'vim')
-                    ["lua_ls"] = function()
-                        lspconfig.lua_ls.setup({
-                            capabilities = capabilities,
-                            settings = {
-                                Lua = { diagnostics = { globals = { "vim" } } },
-                            },
-                        })
-                    end,
-                }
-            })
+								{
+									name = "JavaSE-1.8",
+									path = java8_path,
+								},
+							},
+						},
+					},
+				},
+			})
 
-            -- =========================================================
-            -- 4. DISPARAR JAVA MANUALMENTE
-            -- =========================================================
-            -- Como lo quitamos de Mason-LSPConfig para evitar conflictos,
-            -- debemos iniciarlo explícitamente aquí. nvim-java ya preparó el terreno.
-            lspconfig.jdtls.setup({
-                capabilities = capabilities,
-                settings = {
-                    java = {
-                        configuration = {
-                            runtimes = {
-                                {
-                                    name = "JavaSE-25",
-                                    path = "/usr/lib/jvm/java-25-openjdk",
-                                    default = true,
-                                },
-                                {
-                                    name = "JavaSE-1.8",
-                                    path = "/usr/lib/jvm/java-8-openjdk",
-                                }
-                            }
-                        }
-                    }
-                }
-            })
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 
-            -- =========================================================
-            -- 5. CONFIGURAR AUTOCOMPLETADO (CMP)
-            -- =========================================================
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            
-            require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    ['<Tab>'] = cmp.mapping.select_next_item(),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                }, {
-                    { name = 'buffer' },
-                })
-            })
-        end,
-    }
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
